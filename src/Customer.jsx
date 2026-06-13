967fe9e51535a13e52eb5ef3f34468f84d185f20
Kavin <kavunmail0@gmail.com>
2026-06-13 06:41:46 +0530

    img

--- src/Customer.jsx
+++ src/Customer.jsx
@@ -253,7 +253,17 @@ border: "2px solid #fed7aa",
               boxShadow:
                 "0 2px 10px rgba(0,0,0,0.1)"
             }}
-          >
+          ><img
+  src={item.imageUrl}
+  alt={item.name}
+  style={{
+    width: "100%",
+    height: "160px",
+    objectFit: "cover",
+    borderRadius: "12px",
+    marginBottom: "10px"
+  }}
+/>
             <h3>{item.name}</h3>
 
             <p
